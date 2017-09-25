8f539526885fb53bc74e994ae319646e14f95f56
NatashaKSS <xnagol@gmail.com>
2017-09-26 00:55:08 +0800

    Added tabs for admins

---
+++ vdxn/application/view/dashboard/admin/statstab.php
@@ -0,0 +1,19 @@
+<div class="container">
+  <div class="col-md-3">
+    <?php
+      echo "[SOME STATS FOR ADMIN]";
+    ?>
+  </div>
+  <div class="col-md-9">
+    <?php
+      echo $name . " " . $last_name . "<br/>";
+      echo $email . "<br/>";
+      echo $phone . "<br/>";
+      echo $rating . "<br/>";
+    ?>
+    <button class="btn btn-embossed btn-warning">
+      Change Password
+    </button>
+  </div>
+
+</div>
---
+++ vdxn/application/view/dashboard/admin/taskstab.php
@@ -0,0 +1,8 @@
+<section style="margin: 0 1.5em 0 1.5em;">
+  <h5 class="page-header">
+    Manage Tasks
+    <small style="color: #959595; margin-left: 1em;">
+      <?php echo $num_ongoing_tasks . ' / ' . $num_total_tasks?>
+    </small>
+  </h5>
+</section>
