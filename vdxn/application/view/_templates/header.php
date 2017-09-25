e1280b5f6756553697ea40f2fa16a7b17a393e60
NatashaKSS <xnagol@gmail.com>
2017-09-25 12:32:23 +0800

    Updated CSS + JS dependencies

--- vdxn/application/view/_templates/header.php
+++ vdxn/application/view/_templates/header.php
@@ -12,16 +12,37 @@
 
     <!-- CSS -->
     <link href="/public/css/style.css" rel="stylesheet">
-    <link href="/public/flat-ui-bootstrap-template/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/public/flat-ui-bootstrap-template/css/flat-ui.min.css" rel="stylesheet">
+    <link href="/public/flat-ui-bootstrap-template/dist/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/public/flat-ui-bootstrap-template/dist/css/flat-ui.min.css" rel="stylesheet">
 </head>
 <body>
-    <!-- navigation -->
-    <div class="navigation">
-        <a href="<?php echo URL; ?>">Home</a>
-        <a href="<?php echo URL; ?>dashboard">Dashboard</a>
-        <a href="<?php echo URL; ?>tasks">Tasks</a>
-        <a href="<?php echo URL; ?>mytasks">MyTasks</a>
-        <a href="<?php echo URL; ?>settings">Settings</a>
-        <a href="<?php echo URL; ?>auth">Login</a>
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+        <span class="sr-only">Toggle navigation</span>
+      </button>
+      <a class="navbar-brand" href="#">VDXN</a>
     </div>
+    <div class="collapse navbar-collapse" id="navbar-collapse-01">
+      <ul class="nav navbar-nav">
+        <li class="active">
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/dashboard">Dashboard</a>
+        </li>
+        <li>
+          <a href="/tasks">Tasks</a>
+        </li>
+        <li>
+          <a href="/mytasks">MyTasks</a>
+        </li>
+        <li>
+          <a href="/settings">Settings</a>
+        </li>
+        <li>
+          <a href="/auth">Login</a>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+</nav><!-- /navbar -->
