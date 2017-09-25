4ae0c122092d8e63b3401cf23c7e99f8f2841af9
NatashaKSS <xnagol@gmail.com>
2017-09-25 12:36:48 +0800

    Added skeleton User Dashboard design

--- vdxn/application/view/dashboard/index.php
+++ vdxn/application/view/dashboard/index.php
@@ -1,6 +1,23 @@
 <div class="container">
-    <h1>Dashboard</h1>
-    <p>
-      Contains information related to the user. Only available if authenticated
-    </p>
+  <nav class="navbar navbar-inverse" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+        <span class="sr-only">Toggle navigation</span>
+      </button>
+      <a class="navbar-brand" href="#">My Dashboard</a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-collapse-01">
+      <ul class="nav navbar-nav">
+        <li class="active">
+            <a href="#fakelink">My Profile</a>
+        </li>
+        <li>
+            <a href="#fakelink">My Tasks</a>
+        </li>
+        <li>
+            <a href="#fakelink">My Stats</a>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </nav><!-- /navbar -->
 </div>
