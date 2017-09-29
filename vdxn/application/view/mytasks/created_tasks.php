e33db1d04e95394698f2ba1b2a6e9030d1792b92
SmokinClove <van.tuong.nguyen@hotmail.com>
2017-09-29 11:55:55 +0800

    Separate the table into another file

---
+++ vdxn/application/view/mytasks/created_tasks.php
@@ -0,0 +1,17 @@
+<?php
+  echo '<table border="1">';
+  echo '<tr><th>#</th><th>Title</th><th>Description</th><th>Created At</th>
+        <th>Last Updated At</th><th>Event Date</th>
+        <th>Min Bid</th><th>Max Bid</th><th>Created by</th><th>Assigned To</th><th>Creator Rating</th><th>Assignee Rating</th></tr>';
+  foreach($user_tasks as $task) {
+    echo '<tr>';
+    foreach($task as $item) {
+      echo "<td>$item</td>";
+    }
+    echo "<td><a href='/tasks/task/".$task->{'id'}."'>Link</a></td>";
+    echo "<td><a href='/tasks/edittask/".$task->{'id'}."'>Edit</a></td>";
+    echo "<td><a href='/tasks/bids/".$task->{'id'}."'>Bidding Details</a></td>";
+    echo '</tr>';
+  }
+  echo '</table>';
+?>
--- vdxn/application/view/mytasks/index.php
+++ vdxn/application/view/mytasks/index.php
@@ -1,22 +1,9 @@
 <div class="container">
     <h1>My Tasks</h1>
     <a href="/tasks/newtask">New</a>
-    <?php
-      echo '<div class="tasks__tabs-container"><span class="tasks__tab">Created by Me</span><span class="tasks__tab">Bidded</span></div>';
-      echo '<table border="1">';
-      echo '<tr><th>#</th><th>Title</th><th>Description</th><th>Created At</th>
-            <th>Last Updated At</th><th>Event Date</th>
-            <th>Min Bid</th><th>Max Bid</th><th>Created by</th><th>Assigned To</th><th>Creator Rating</th><th>Assignee Rating</th></tr>';
-      foreach($user_tasks as $task) {
-        echo '<tr>';
-        foreach($task as $item) {
-          echo "<td>$item</td>";
-        }
-        echo "<td><a href='/tasks/task/".$task->{'id'}."'>Link</a></td>";
-        echo "<td><a href='/tasks/edittask/".$task->{'id'}."'>Edit</a></td>";
-        echo "<td><a href='/tasks/bids/".$task->{'id'}."'>Bidding Details</a></td>";
-        echo '</tr>';
-      }
-      echo '</table>';
-    ?>
+    <div class="tasks__tabs-container">
+      <span class="tasks__tab tasks__tab--selected">Created by Me</span>
+      <span class="tasks__tab">Bidded</span>
+    </div>
+    <?php include ("created_tasks.php") ?>
 </div>
