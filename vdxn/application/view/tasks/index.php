9a171b447831642235f7a8c898638187152a6001
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-09-30 22:57:28 +0800

    add dynamic search using ajax

--- vdxn/application/view/tasks/index.php
+++ vdxn/application/view/tasks/index.php
@@ -1,23 +1,86 @@
 <div class="container">
     <h1>Tasks</h1>
     <p>
-      Content should contain all tasks available in the system that is
-      available for bidding
+        Content should contain all tasks available in the system that is
+        available for bidding
     </p>
-    <?php
-      echo '<table border="1">';
-      echo '<tr><th>#</th><th>Title</th><th>Description</th><th>Create time</th>
-            <th>Updated time</th><th>Expiry</th><th>Event Date</th>
-            <th>Min Bid</th><th>Max Bid</th><th>Tasker</th></tr>';
-      foreach($tasks as $task) {
-        echo '<tr>';
-        foreach($task as $item) {
-          echo "<td>$item</td>";
-        }
-        // TODO: to re-implement the link to Task page
-        //echo "<td><a href='/tasks/task/".$task->{'id'}."'>Link</a></td>";
-        echo '</tr>';
-      }
-      echo '</table>';
-    ?>
+    <div class="row">
+        <div class="col-xm">
+            <p>Type a name to begin searching</p>
+            <form class="form-horizontal" name="search" role="form" method="POST">
+                <div class="input-group col-xm">
+                    <input type="text" id="name" name="name" class="form-control" placeholder="Search by name..."
+                           autocomplete="off"/>
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-default btnSearch">
+                            <span class="glyphicon glyphicon-search"> </span>
+                        </button>
+                    </span>
+                    <!--p>Suggestions: <span id="txtHint"></span></p-->
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row mt">
+        <div class="col-lg-12">
+            <div class="content-panel tablesearch">
+                    <table id="resultTable" class="table table-bordered table-hover table-condensed">
+                        <thead>
+                        <tr><th>Title</th><th>Description</th><th>Create time</th>
+                            <th>Updated time</th><th>Expiry</th><th>Event Date</th>
+                            <th>Min Bid</th><th>Max Bid</th><th>Tasker</th>
+                        </tr>
+                        </thead><tbody>
+                        <?php
+                        foreach ($tasks as $task) {
+                            echo '<tr>';
+                            foreach ($task as $item) {
+                                echo "<td>$item</td>";
+                            }
+                            // TODO: to re-implement the link to Task page
+                            //echo "<td><a href='/tasks/task/".$task->{'id'}."'>Link</a></td>";
+                            echo '</tr>';
+                        }
+                        ?></tbody></table>
+            </div><!-- /content-panel -->
+        </div><!-- /col-lg-4 -->
+    </div><!-- /row -->
 </div>
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        function search() {
+            var query_value = $('input#name').val();
+            console.log(query_value);
+
+                $.ajax({
+                    type: "POST",
+                    url: "//192.168.33.66/js/tasksearch.php",
+                    data: { query: query_value },
+                    cache: false,
+                    success: function(html){
+                        $("table#resultTable tbody").html(html);
+                        console.log(html);
+                    }
+                });
+
+
+        }
+
+        $('input#name').on("keyup input", function(e) {
+
+            // Set Search String
+            var search_string = $(this).val();
+
+            // Do Search
+                search();
+                $(".tablesearch").fadeIn(300);
+                console.log('key is' + search_string);
+        });
+
+    });
+</script>
+
---
+++ vdxn/public/js/tasksearch.php
@@ -0,0 +1,114 @@
+<?php
+// Output HTML formats
+global $test_db;
+
+$test_db = new mysqli();
+$test_db->connect('127.0.0.1', 'root', '12345678', 'mini');
+$test_db->set_charset("utf8");
+
+//	Check Connection
+if ($test_db->connect_errno) {
+    printf("Connect failed: %s\n", $test_db->connect_error);
+    exit();
+}
+
+$html = '<tr><th>Title</th><th>Description</th><th>Create time</th>
+                            <th>Updated time</th><th>Expiry</th><th>Event Date</th>
+                            <th>Min Bid</th><th>Max Bid</th><th>Tasker</th>
+                        </tr>';
+
+// Get the Search
+
+$search_string = preg_replace("/[^A-Za-z0-9]/", " ", $_POST['query']);
+$search_string = $test_db->real_escape_string($search_string);
+
+// Check if length is more than 1 character
+if (strlen($search_string) >= 1 && $search_string !== ' ') {
+    // Query
+    $query = 'SELECT * FROM Task WHERE title LIKE "%'.$search_string.'%"';
+
+    // Do the search
+    $result = $test_db->query($query);
+    while($results = $result->fetch_array()) {
+        $result_array[] = $results;
+    }
+
+    // Check for results
+    if (isset($result_array)) {
+        foreach ($result_array as $result) {
+            // Output strings and highlight the matches
+            $d_title = preg_replace("/" . $search_string . "/i", "<b>" . $search_string . "</b>", $result['title']);
+            $d_description = $result['description'];
+            $d_createTime = $result['created_at'];
+            $d_updateTime = $result['updated_at'];
+            $d_expiry = $result['end_at'];
+            $d_eventDate = $result['start_at'];
+            $d_minBid = $result['min_bid'];
+            $d_maxBid = $result['max_bid'];
+            $d_tasker = $result['creater_username'];
+            // Replace the items into above HTML
+            $o = str_replace('Title', $d_title, $html);
+            $o = str_replace('Description', $d_description, $o);
+            $o = str_replace('Create time', $d_createTime, $o);
+            $o = str_replace('Updated time', $d_updateTime, $o);
+            $o = str_replace('Expiry', $d_expiry, $o);
+            $o = str_replace('Event Date', $d_eventDate, $o);
+            $o = str_replace('Min Bid', $d_minBid, $o);
+            $o = str_replace('Max Bid', $d_maxBid, $o);
+            $o = str_replace('tasker', $d_tasker, $o);
+            // Output it
+            echo($o);
+
+        }
+    } else {
+            // Replace for no results
+            $o = str_replace('Title', '<span class="label label-danger">No Names Found</span>', $html);
+            $o = str_replace('Description', '', $o);
+            $o = str_replace('Create time', '', $o);
+            $o = str_replace('Updated time', '', $o);
+            $o = str_replace('Expiry', '', $o);
+            $o = str_replace('Event Date', '', $o);
+            $o = str_replace('Min Bid', '', $o);
+            $o = str_replace('Max Bid', '', $o);
+            $o = str_replace('tasker', '', $o);
+            // Output
+            echo($o);
+        }
+    }
+
+else {
+    $query = 'SELECT * FROM Task';
+
+// Do the search
+    $result = $test_db->query($query);
+    while ($results = $result->fetch_array()) {
+        $result_array[] = $results;
+    }
+
+// Check for results
+    foreach ($result_array as $result) {
+        // Output strings and highlight the matches
+        $d_title = preg_replace("/" . $search_string . "/i", "<b>" . $search_string . "</b>", $result['title']);
+        $d_description = $result['description'];
+        $d_createTime = $result['created_at'];
+        $d_updateTime = $result['updated_at'];
+        $d_expiry = $result['end_at'];
+        $d_eventDate = $result['start_at'];
+        $d_minBid = $result['min_bid'];
+        $d_maxBid = $result['max_bid'];
+        $d_tasker = $result['creater_username'];
+        // Replace the items into above HTML
+        $o = str_replace('Title', $d_title, $html);
+        $o = str_replace('Description', $d_description, $o);
+        $o = str_replace('Create time', $d_createTime, $o);
+        $o = str_replace('Updated time', $d_updateTime, $o);
+        $o = str_replace('Expiry', $d_expiry, $o);
+        $o = str_replace('Event Date', $d_eventDate, $o);
+        $o = str_replace('Min Bid', $d_minBid, $o);
+        $o = str_replace('Max Bid', $d_maxBid, $o);
+        $o = str_replace('tasker', $d_tasker, $o);
+        // Output it
+        echo($o);
+    }
+}
+?>
