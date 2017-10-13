41db44e84df0c6cfe0ccbd26169dd7f1cb512132
namjjd <namjjd@gmail.com>
2017-10-13 23:38:00 +0800

    updated generation code

--- faker/t.php
+++ faker/t.php
@@ -7,7 +7,7 @@
     $PASSWORDFORALL = 'vanisgay';
 
     // Generate users
-for($q = 0; $q < 1000; $q++) {
+/*for($q = 0; $q < 100; $q++) {
     $time = date_format($faker->dateTimeThisDecade($max="now"), "Y-m-d H:i:s");
     $password = password_hash($PASSWORDFORALL, PASSWORD_DEFAULT);
 
@@ -36,5 +36,49 @@ for($q = 0; $q < 1000; $q++) {
     )";
 
     echo $template;
+}
+*/
+// Generate tasks
+
+for($i = 0; $i < 100; $i++) {
+  $time = date_format($faker->dateTimeThisDecade($max="now"), "Y-m-d H:i:s");
+  $biasedminbid = $faker->biasedNumberBetween($min = 1, $max = 40, $function = 'sqrt');
+  $biasedmaxbid = $faker->biasedNumberBetween($min = 41, $max = 120, $function = 'sqrt');
+  $username = 'asdfasf';
+
+  $template = "INSERT INTO `mini`.`Task` (
+    `title`,
+    `description`,
+    `created_at`,
+    `updated_at`,
+    `start_at`,
+    `end_at`,
+    `min_bid`,
+    `max_bid`,
+    `creator_username`,
+    `assignee_username`,
+    `deleted_at`,
+    `creator_rating`,
+    `assignee_rating`,
+    `completed_at`,
+    `remarks`)
+    VALUES (
+      '$faker->jobTitle',
+      '$faker->paragraph',
+      '$time',
+      '$time',
+      '2017-10-16 00:00:00',
+      '2017-10-21 00:00:00',
+      '$biasedminbid',
+      '$biasedmaxbid',
+      '$username',
+      '',
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL);";
+
+      echo $template;
 }
 ?>
