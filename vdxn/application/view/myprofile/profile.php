fe62607cd710882a7e0d3605162bddda85ae53fe
NatashaKSS <xnagol@gmail.com>
2017-10-25 16:33:42 +0800

    Added first name, last name & created on attrs to public profile

--- vdxn/application/view/myprofile/profile.php
+++ vdxn/application/view/myprofile/profile.php
@@ -3,7 +3,24 @@
     <img src="<?php echo $pictureLink ?>" height=150 width=150 style="margin-bottom: 1em;"/>
   </div>
   <div class="col-md-9">
-    <h4><?php echo $user->{'username'}?></h4>
+    <h4>
+      <?php echo $user->{'first_name'} . ' ' .
+                 $user->{'last_name'} . ' (' .
+                 $user->{'username'} . ')'
+      ?>
+    </h4>
+
+    <br/><small><b>PROFILE</b></small><br/>
+    <table class="table user-contact-information-table">
+      <tr>
+        <td>Created on:</td>
+        <td><?php echo $user->{'created_at'} ?></td>
+      </tr>
+      <tr>
+        <td>Type:</td>
+        <td><?php echo $user->{'user_type'} ?></td>
+      </tr>
+    </table>
 
     <br/><small><b>CONTACT INFORMATION</b></small><br/>
     <table class="table user-contact-information-table">
