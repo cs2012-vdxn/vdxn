418cfbdd310e4439a78db70bfa25dca99e37851c
NatashaKSS <xnagol@gmail.com>
2017-11-01 22:50:04 +0800

    Updated login screen UI look

--- vdxn/application/view/login/index.php
+++ vdxn/application/view/login/index.php
@@ -1,9 +1,25 @@
 <div class="container">
-  <p>Login</p>
-  <form action="<?php echo URL; ?>login/submitForm" method="POST">
-  Name: <input type="text" name="username"><br>
-  Password: <input type="password" name="password"><br>
-  <input type="submit">
-  <p>Create an account <a href="<?php echo URL;?>signup">here</a>.</p>
-</form>
+  <div class="login">
+    <div style="background-color: #1abc9c; min-height: 473px; padding: 100px 199px 33px 199px;">
+      <div class="login-form">
+        <div class="form-group">
+          <form action="<?php echo URL; ?>login/submitForm" method="POST">
+            <div class="form-group">
+              <input type="text" name="username" class="form-control login-field" value="" placeholder="Enter your name" id="login-name" autocomplete="off" style="background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
+              <label class="login-field-icon fui-user" for="login-name"></label>
+            </div>
+            <div class="form-group">
+              <input type="password" name="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" autocomplete="off" style="background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+              <label class="login-field-icon fui-lock" for="login-pass"></label>
+            </div>
+            <input class="btn btn-primary btn-lg btn-block" type="submit" value="Log in" />
+            <p style="color: #000000; text-align: center;">
+              <br />
+              Create an account <a href="<?php echo URL;?>signup">here</a>.
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
